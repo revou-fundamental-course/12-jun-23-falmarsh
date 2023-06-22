097b92cc54620085a30e19f2f74591d5417d18c1
Almas Farah <68049040+falmarsh@users.noreply.github.com>
2023-06-22 18:41:52 +0700

    Kalkulator suhu v.1.00
    honestly, i still don't understand about the basic javascript. I'll try to update again in my own repo...

---
+++ js/script.js
@@ -0,0 +1,9 @@
+
+// bakal update project lagi kalo udah lancar basic-basicnya javascript
+
+/* learning source:
+https://www.w3schools.com/howto/howto_js_temperature_converter.asp
+https://codepen.io/Caparico/pen/AXOBBY
+https://www.rapidtables.com/convert/temperature/fahrenheit-to-celsius.html?x=5
+https://www.calculatorsoup.com/calculators/conversions/temperature.php
+*/
